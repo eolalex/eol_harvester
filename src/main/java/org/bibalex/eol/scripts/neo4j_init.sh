a3735dcb9204c95bb2f12665e997902ac11c9f6e
menna-hedaia <menna.hedaia@bibalex.org>
2021-01-18 15:31:12 +0200

    add neo4j initialization scripts

---
+++ src/main/java/org/bibalex/eol/scripts/neo4j_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# One-time initializations for the Neo4j graph database
+
+cypher-shell <<EOF
+create index on :GNode(node_id);
+create index on :GNode(generated_node_id);
+create (:GlobalUniqueId {count: 0, page_id: 0, trait_id: 0});
+EOF
